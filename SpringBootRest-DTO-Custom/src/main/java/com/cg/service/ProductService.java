68a56820cb89f5bdd28d3b456d1bc43de0191038
MenakshiShendge <121344705+MenakshiShendge@users.noreply.github.com>
2025-02-13 12:25:31 +0530

    Add files via upload

---
+++ SpringBootRest-DTO-Custom/src/main/java/com/cg/service/ProductService.java
@@ -0,0 +1,63 @@
+package com.cg.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.entity.Products;
+import com.cg.repository.ProductRepository;
+@Service
+public class ProductService implements IProductService {
+    
+	List<Products> productList = new ArrayList();
+    @Autowired
+    ProductRepository productRepository;
+	
+	@Override
+	public List<Products> findAllProduct() {
+		return productRepository.findAll() ;
+	}
+	
+	@Override
+	public Optional<Products> findproductbyid(int id){
+		return productRepository.findById(id); 
+	}
+	
+	
+	@Override
+	public List<Products> findproductbyname(String pname) {
+		
+		return productRepository.findByPname(pname);
+	}
+	
+	
+	@Override
+	public void deleteproductbyid(int id) {
+		productRepository.deleteById(id);
+	}
+
+	@Override
+	public Products newproduct(Products products) {
+		return productRepository.save(products);
+		
+	}
+
+	@Override
+	public Products updateproduct(int id, Products products) {
+		Products p=productRepository.findById(id).get();
+		p.setPname(products.getPname());
+		p.setPrice(products.getPrice());
+		return productRepository.save(p);
+	}
+
+	@Override
+	public Long findproductcountbyname(String name) {
+		return productRepository.countByProductName(name);
+	}
+	
+	
+	
+}
